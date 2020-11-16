b07eaa4f00852d6ed51538ce0ed09174983042d4
Ioannis Cherouvim <743305+cherouvim@users.noreply.github.com>
2020-11-16 21:33:22 +0200

    feat: Empty service worker.

--- app.js
+++ app.js
@@ -2,4 +2,19 @@
   "use strict";
 
   console.log(123);
+
+  function registerServiceWorker() {
+    if (!("serviceWorker" in navigator)) return;
+    console.log("Registering service worker");
+    navigator.serviceWorker.register("/service-worker.js", { scope: "/" }).then(
+      function () {
+        console.log("Service worker has been registered");
+      },
+      function (e) {
+        console.error("Service worker registration failed", e);
+      }
+    );
+  }
+
+  registerServiceWorker();
 })();
